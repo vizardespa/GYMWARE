f37fe58c6cecde3b36f57f793d3b4743ea405539
Martin Toledo <vizardespa@gmail.com>
2014-05-19 18:02:56 -0700

    Base
    Base :D

---
+++ Gymware/Gymware/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gymware.MvcApplication" Language="C#" %>
---
+++ Gymware/Gymware/Models/GYMDB.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'c:\users\vizardespa\documents\visual studio 2012\Projects\Gymware\Gymware\Models\GYMDB.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
