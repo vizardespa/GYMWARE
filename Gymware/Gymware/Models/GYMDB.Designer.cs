792fddf4dc8621d8601e1d86cc17693b580ddabf
Martin Toledo <vizardespa@gmail.com>
2014-05-30 00:00:25 -0700

    *Actualizado .ADO respecto a la base de datos mas actual

--- Gymware/Gymware/Models/GYMDB.Designer.cs
+++ Gymware/Gymware/Models/GYMDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'c:\users\vizardespa\documents\visual studio 2012\Projects\Gymware\Gymware\Models\GYMDB.edmx'. 
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Vizardespa\Documents\Gymware\trunk\Gymware\Gymware\Models\GYMDB.edmx'. 
 // Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
